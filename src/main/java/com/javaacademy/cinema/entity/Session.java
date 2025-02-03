39464cef51e9e351d815845504b4b2d6b7e67255
Alexandwere <lazukov_ao@mail.ru>
2025-02-03 14:23:39 +0300

    chore

--- src/main/java/com/javaacademy/cinema/entity/Session.java
+++ src/main/java/com/javaacademy/cinema/entity/Session.java
@@ -1,11 +1,17 @@
 package com.javaacademy.cinema.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Session {
     private Integer id;
     private LocalDateTime localDateTime;
